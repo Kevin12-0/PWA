0668e50e2aebca0d5ca2a235dd577335907388e0
Kecvin-12-u <65631970+Kevin12-0@users.noreply.github.com>
2023-03-15 16:47:43 -0600

    cache recovery

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 console.log("HELLO WORLD");
+
+const browserInformation = {
+    name: 'Microsoft Edge',
+    version: 108
+};
+
+localStorage.setItem('browser', JSON.stringify(browserInformation));
+
+const value = localStorage.getItem('browser');
+
+self.addEventListener("fetch", event => {
+    async function cacheAndReturnRequest() {
+        // Get the response from the server.
+        const fetchResponse = await fetch(event.request.url);
+        // Open the app's cache.
+        const cache = await caches.open("cache-name");
+        // Put the response in cache.
+        cache.put(event.request.url, fetchResponse.clone());
+        // And return the response.
+        return fetchResponse
+    }
+
+    event.respondWith(cacheAndReturnRequest());
+});
+
+openFileButton.addEventListener("click", async () => {
+    const fileHandles = await window.showOpenFilePicker();
+});
