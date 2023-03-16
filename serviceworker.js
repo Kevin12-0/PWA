3f621fca479925bd902e92f3dbfe599956369d32
Kecvin-12-u <65631970+Kevin12-0@users.noreply.github.com>
2023-03-15 23:53:37 -0600

    serviseworker modify

--- serviceworker.js
+++ serviceworker.js
@@ -1,44 +1,20 @@
 const cache_name = "pwa-cv-v1";
 const urlsToCache = [
-    '/', '/js/index.js',"/index.html"
+    '/', './js/index.js', "/index.html"
 ];
 
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(cache_name)
-            .then(cache => {
-                console.log("cache opened");
-                return cache.addAll(urlsToCache);
-            })
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(staticDevCoffee).then(cache => {
+            cache.addAll(assets);
+        })
     );
 });
 
-
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-
-                const fetchRequest = event.request.clone();
-
-                return fetch(fetchRequest).then(
-                    response => {
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-
-                        const responseToCache = response.clone();
-
-                        caches.open(cache_name)
-                            .then(cache => {
-                                cache.put(event.request, responseToCache)
-                            });
-                        return response;
-                    }
-                )
-            })
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
     )
 });
